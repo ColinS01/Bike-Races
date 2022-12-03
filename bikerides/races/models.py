cdf5a846c65476b73cbc5b2510b34059fd659292
Colin Smith <colinsmith8601@gmail.com>
2022-12-03 15:12:49 -0500

    start user auth

--- bikerides/races/models.py
+++ bikerides/races/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth import authenticate, get_user_model
 
 # Create your models here.
 
@@ -7,7 +10,7 @@ states = (('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkans
 class Rider(models.Model):
     first = models.CharField(max_length=16)
     last = models.CharField(max_length=16)
-    email = models.CharField(max_length=100, default= '')
+    email = models.EmailField(default='')
     password = models.CharField(max_length=16, default='')
     sex = models.CharField(max_length=7)
     age = models.IntegerField()
@@ -27,3 +30,28 @@ class Race(models.Model):
     
     def __str__(self):
         return self.name
+
+class UserForm(ModelForm):
+    class Meta:
+        model = Rider
+        fields = ['first', 'last', 'email', 'password', 'age', 'sex']
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    def clean(self, *args, **kwargs):
+
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                raise forms.ValidationError('User does not exist')
+
+            if not user.check_password(password):
+                raise forms.ValidationError('Incorrect password')
+                
+        return super(UserLoginForm, self).clean(*args, **kwargs)
--- bikerides/races/views.py
+++ bikerides/races/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'races/index.html')
 
 def sign_up(request):
+    if request.POST == 'POST':
+        form = UserForm()
+        if form.is_valid():
+            form.save()
+        return render(request, 'races/dashboard.html')
+
     return render(request, 'races/signup.html')
 
 def login(request):
