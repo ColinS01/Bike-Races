1dcec4b70c51441ed94169c67860bd973c6f327a
Colin Smith <colinsmith8601@gmail.com>
2022-12-03 13:26:52 -0500

    models created

--- bikerides/races/urls.py
+++ bikerides/races/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  
+    path('login/', views.login, name='login'),  
+    path('signup/', views.sign_up, name='sign up'),
+    path('create/', views.create_race, name='create'),
+    path('seeraces/', views.see_races, name='races'),
+    path('register/', views.register, name='register'),
+    path('myraces/', views.my_races, name='my races'),
 ]
